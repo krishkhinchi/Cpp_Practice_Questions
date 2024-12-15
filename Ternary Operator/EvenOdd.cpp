e839f53407ad2f4f76bc3d45189184d545bc1c0c
krishhackz <krishhackz.in@gmail.com>
2024-12-15 16:54:42 +0530

    Check a number is even or odd using Ternary Operator

---
+++ Ternary Operator/EvenOdd.cpp
@@ -0,0 +1,11 @@
+// Check a number is even or odd using Ternary Operator
+
+#include<iostream>
+using namespace std;
+int main(){
+    int Input;
+    cout << "Enter a Number: ";
+    cin >> Input;
+    (Input%2==0) ? cout << "Even" : cout << "Odd";
+    return 0;
+}
