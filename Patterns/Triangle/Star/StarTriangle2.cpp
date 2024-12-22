91f99ba700206768cfd41b3b355fdf9353e89520
krishhackz <krishhackz.in@gmail.com>
2024-12-23 00:04:53 +0530

    Star Triangle #2

---
+++ Patterns/Triangle/Star/StarTriangle2.cpp
@@ -0,0 +1,18 @@
+// *****
+// ****
+// ***
+// **
+// *
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=0;i<5;i++){
+        for(j=5;j>i;j--){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
