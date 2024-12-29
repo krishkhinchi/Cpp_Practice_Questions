43a569133be7f2102362d3c24b98bbfef2a4a548
krishhackz <krishhackz.in@gmail.com>
2024-12-29 23:15:25 +0530

    Star Square Pattern

---
+++ Patterns/Square/StarSquare.cpp
@@ -0,0 +1,16 @@
+// *****
+// *****
+// *****
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=0;i<3;i+=1){
+        for(j=0;j<5;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
