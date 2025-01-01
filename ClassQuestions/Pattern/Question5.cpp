6c7342b6e30bdc46fc8cfad696ea1b4f36acd75a
krishhackz <krishhackz.in@gmail.com>
2025-01-01 22:55:41 +0530

    Star Right Angle Triangle Inverse

---
+++ ClassQuestions/Pattern/Question5.cpp
@@ -0,0 +1,16 @@
+//     *
+//    **
+//   ***
+//  ****
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j,k;
+    for(i=0;i<4;i++){
+        for(j=4;j>i;j--) cout << " ";
+        for(k=0;k<=i;k++) cout << "*";
+        cout << endl;
+    }
+    return 0;
+}
