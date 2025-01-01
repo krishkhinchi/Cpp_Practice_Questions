8a8695410603fae957f52776e2cc5bded3c387ad
krishhackz <krishhackz.in@gmail.com>
2025-01-01 22:08:45 +0530

    Star Square

---
+++ ClassQuestions/Pattern/Question1.cpp
@@ -0,0 +1,15 @@
+// ****
+// ****
+// ****
+// ****
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=0;i<4;i+=1){
+        for(j=0;j<4;j+=1) cout << "*";    
+        cout << endl;
+    }
+    return 0;
+}
---
+++ ClassQuestions/Pattern/StarSquare.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=0;i<4;i+=1){
+        for(j=0;j<4;j+=1) cout << "*";    
+        cout << endl;
+    }
+    return 0;
+}
