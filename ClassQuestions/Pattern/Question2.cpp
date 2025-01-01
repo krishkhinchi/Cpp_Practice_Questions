96df46a5c21d0253e90c7b3b96d37862d4909822
krishhackz <krishhackz.in@gmail.com>
2025-01-01 22:12:41 +0530

    Hollow Star Square

---
+++ ClassQuestions/Pattern/Question2.cpp
@@ -0,0 +1,15 @@
+// ****
+// *  *
+// *  *
+// ****
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=0;i<4;i++){
+        for(j=0;j<4;j++) if(i==0||i==3||j==0||j==3) cout<<"*"; else cout<<" ";
+        cout << endl;
+    }
+    return 0;
+}
