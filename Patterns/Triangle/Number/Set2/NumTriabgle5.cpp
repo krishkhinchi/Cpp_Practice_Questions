2b446e50cacf75df7aef612eb981457aecdf1cbf
krishhackz <krishhackz.in@gmail.com>
2025-01-05 18:15:22 +0530

    Vertical Number Triangle #5

---
+++ Patterns/Triangle/Number/Set2/NumTriabgle5.cpp
@@ -0,0 +1,22 @@
+// 0
+// 12
+// 345
+// 6789
+
+#include <iostream>
+using namespace std;
+int main(){
+    int Num=0;
+    int i=0;
+    while(i<4){
+        int j=0;
+        while(j<=i){
+            cout << Num;
+            Num+=1;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
