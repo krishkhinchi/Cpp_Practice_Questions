4b62b680624f5ead582202123275b226d914e1bf
krishhackz <krishhackz.in@gmail.com>
2025-01-06 00:29:30 +0530

    Odd Number Triangle #3

---
+++ Patterns/Triangle/Number/Odd/OddNumber3.cpp
@@ -0,0 +1,22 @@
+//  1357
+//   135
+//    13
+//     1
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j,k;
+    for(i=0;i<4;i++){
+        int Odd=1;
+        for(j=0;j<=i;j++){
+            cout << " ";
+        }
+        for(k=4;k>i;k--){
+            cout << Odd;
+            Odd+=2;
+        }
+        cout << endl;
+    }
+    return 0;
+}
