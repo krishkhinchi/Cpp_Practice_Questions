a712dcca52bd437e25bcea41e36bc780241c20f3
krishhackz <krishhackz.in@gmail.com>
2025-01-26 23:25:41 +0530

    Equilateral Star Triangle

---
+++ Patterns/Triangle/Equilateral/Star.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=5;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<x;j++) cout << " ";
+        x--;
+        for(int k=0;k<=i;k++) cout << " *";
+        cout << endl;
+    }
+    return 0;
+}
