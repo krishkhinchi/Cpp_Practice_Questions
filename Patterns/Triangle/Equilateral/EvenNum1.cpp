9b44585555c19f71d40725174ea41a6f6f60096e
krishhackz <krishhackz.in@gmail.com>
2025-01-26 23:29:26 +0530

    Equilateral Even Number Triangle #1

---
+++ Patterns/Triangle/Equilateral/EvenNum1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=4,num=2;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<x;j++) cout << " ";
+        x--;
+        for(int k=0;k<=i;k++) cout << " " << num;
+        num+=2;
+        cout << endl;
+    }
+    return 0;
+}
