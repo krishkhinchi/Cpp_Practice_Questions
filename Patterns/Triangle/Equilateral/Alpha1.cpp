6b3b51a576f285029323f79aa7444fc38de1a00f
krishhackz <krishhackz.in@gmail.com>
2025-01-27 00:55:11 +0530

    Equilateral Alphabet Triangle #1

--- Patterns/Triangle/Equilateral/Alpha1.cpp
+++ Patterns/Triangle/Equilateral/Alpha1.cpp
@@ -1,9 +1,15 @@
+//       A
+//      B B
+//     C C C
+//    D D D D
+//   E E E E E
+
 #include <iostream>
 using namespace std;
 int main(){
-    int x=4;
+    int x=5;
     char ch='A';
-    for(int i=0;i<4;i++){
+    for(int i=0;i<5;i++){
         for(int j=0;j<x;j++) cout << " ";
         x--;
         for(int k=0;k<=i;k++) cout << " " << ch;
